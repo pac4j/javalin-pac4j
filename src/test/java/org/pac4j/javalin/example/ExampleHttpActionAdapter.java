065d529694a45910c268a3c0563f270428fb51ae
Maximilian Hippler <maximilian@hiiw.de>
2020-04-22 20:40:50 +0200

    Added a message to the ExampleHttpActionAdapter

--- src/test/java/org/pac4j/javalin/example/ExampleHttpActionAdapter.java
+++ src/test/java/org/pac4j/javalin/example/ExampleHttpActionAdapter.java
@@ -11,8 +11,8 @@ public class ExampleHttpActionAdapter extends JavalinHttpActionAdapter {
     @Override
     public Void adapt(HttpAction action, JavalinWebContext context) {
         switch(action.getCode()){
-            case HttpConstants.UNAUTHORIZED: throw new UnauthorizedResponse();
-            case HttpConstants.FORBIDDEN: throw new ForbiddenResponse();
+            case HttpConstants.UNAUTHORIZED: throw new UnauthorizedResponse("Unauthorized - Please Login first");
+            case HttpConstants.FORBIDDEN: throw new ForbiddenResponse("Forbidden - You don't have access to this resource");
             default: return super.adapt(action, context);
         }
     }
