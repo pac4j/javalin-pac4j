7c9db56e20a0329acc057078ca440a35a3591ca6
Maximilian Hippler <maximilian@hiiw.de>
2020-04-22 20:56:55 +0200

    Logic optimization in JavalinHttpActionAdapter

--- src/main/java/org/pac4j/javalin/JavalinHttpActionAdapter.java
+++ src/main/java/org/pac4j/javalin/JavalinHttpActionAdapter.java
@@ -1,12 +1,14 @@
 package org.pac4j.javalin;
 
+import io.javalin.http.BadRequestResponse;
 import io.javalin.http.ForbiddenResponse;
 import io.javalin.http.UnauthorizedResponse;
 import org.pac4j.core.context.HttpConstants;
-import org.pac4j.core.exception.TechnicalException;
 import org.pac4j.core.exception.http.HttpAction;
+import org.pac4j.core.exception.http.WithContentAction;
 import org.pac4j.core.exception.http.WithLocationAction;
 import org.pac4j.core.http.adapter.HttpActionAdapter;
+import org.pac4j.core.util.CommonHelper;
 
 /**
  * @author Maximilian Hippler
@@ -17,16 +19,27 @@ public class JavalinHttpActionAdapter implements HttpActionAdapter<Void, Javalin
 
     @Override
     public Void adapt(HttpAction action, JavalinWebContext context) {
-        if (action instanceof WithLocationAction) {
-            context.getJavalinCtx().redirect(((WithLocationAction) action).getLocation());
+        CommonHelper.assertNotNull("action", action);
+        CommonHelper.assertNotNull("context", context);
+
+        if(action instanceof WithContentAction){
+            context.getJavalinCtx().status(action.getCode());
+            context.getJavalinCtx().result(((WithContentAction) action).getContent());
+            return null;
+        } else if (action instanceof WithLocationAction) {
+            context.getJavalinCtx().redirect(((WithLocationAction) action).getLocation(), action.getCode());
             return null;
         } else switch (action.getCode()) {
             case HttpConstants.UNAUTHORIZED:
                 throw new UnauthorizedResponse();
             case HttpConstants.FORBIDDEN:
                 throw new ForbiddenResponse();
-            default:
-                throw new TechnicalException("No action provided");
+            case HttpConstants.BAD_REQUEST:
+                throw new BadRequestResponse();
+            default: {
+                context.getJavalinCtx().status(action.getCode());
+                return null;
+            }
         }
     }
 }
